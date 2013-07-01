9a4d2a23c6c3892e38b9b29bea84d2f3ca2bd7f2
Stefan de Konink <stefan@konink.de>
2013-07-01 10:41:43 +0200

    Update

--- openebs2/settings.py
+++ openebs2/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Stefan de Konink', 'stefan@opengeo.nl'),
 )
 
 MANAGERS = ADMINS
@@ -121,10 +121,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'openebs',
 )
 
 # A sample logging configuration. The only tangible logging
