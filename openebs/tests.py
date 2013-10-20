e1387f4ae8a194645077a1b172415224eef122d4
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-10-20 15:15:34 +0200

    Override delete method with custom method which doesn't actually delete
    TODO: Add new force_delete method

--- openebs/tests.py
+++ openebs/tests.py
@@ -4,13 +4,56 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from datetime import timedelta
 from django.test import TestCase
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+from models import Kv15Stopmessage
 
+class TestKv15MessageModel(TestCase):
+    user = None
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        self.user = User.objects.create_user("test")
+
+    def create_message_default(self):
+        msg = Kv15Stopmessage()
+        msg.user = self.user
+        msg.messagecodedate = now().date().isoformat()
+        msg.messageendtime = msg.messagestarttime + timedelta(1)
+        msg.measurecontent = "This is a test message"
+        return msg
+
+    def test_new_message_twice(self):
+        msg = self.create_message_default()
+        msg.save()
+        self.assertEqual(msg.messagecodenumber, 1)
+
+        msg = self.create_message_default()
+        msg.save()
+
+        self.assertEqual(msg.messagecodenumber, 2)
+
+    def test_new_message_per_day(self):
+        msg = self.create_message_default()
+        msg.save()
+        self.assertEqual(msg.messagecodenumber, 1)
+
+        # Create another message and add a day
+        msg = self.create_message_default()
+        date = now() + timedelta(1)
+        msg.messagecodedate = date.date().isoformat()
+        msg.save()
+
+        self.assertEqual(msg.messagecodenumber, 1)
+
+    def test_delete(self):
+        msg = self.create_message_default()
+        msg.save()
+        self.assertFalse(msg.isdeleted)
+        msg_pk = msg.pk
+        msg.delete()
+
+        # Delete should be nothing more than triggering our flag
+        self.assertTrue(msg.isdeleted)
+        self.assertEqual(msg_pk, msg.pk)
