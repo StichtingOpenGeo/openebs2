6e840e1956f6a83582d385a75e2310344ee8db21
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-10-31 08:20:44 +0100

    Improve selection to allow deselecting from line too
    Needs work when we have non-unique stop-ids

--- openebs2/local_settings.py
+++ openebs2/local_settings.py
@@ -1,11 +1,10 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'openebs',              # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', 
+        'NAME': 'openebs',
         'USER': 'openebs',
         'PASSWORD': 'openebs',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'HOST': 'localhost',       
+        'PORT': '5432',    
     }
 }
