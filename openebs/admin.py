d63be7285c9cbcc9a53b02eeb7524b0700a9cac9
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-11-01 21:36:02 +0100

    Fix permissions in the admin to work beter

--- openebs/admin.py
+++ openebs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Permission, Group
 from django.contrib.sites.models import Site
+from django.forms import forms
 
 from django.utils.translation import ugettext_lazy as _
 from openebs.models import Kv15Stopmessage, Kv15Log, Kv15Scenario, UserProfile, Kv15MessageStop, Kv15ScenarioMessage
@@ -31,17 +32,34 @@ class Kv15ScenarioAdmin(admin.ModelAdmin):
 admin.site.register(Kv15Scenario, Kv15ScenarioAdmin)
 admin.site.register(Kv15Log)
 
+class PermissionFilterMixin(object):
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name in ('permissions', 'user_permissions'):
+            qs = kwargs.get('queryset', db_field.rel.to.objects)
+            if not request.user.is_superuser: # This hides all the other permissions if we're "just" staff
+                qs = qs.exclude(name__startswith="Can")
+            kwargs['queryset'] = qs
+
+        return super(PermissionFilterMixin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
+class MyGroupAdmin(PermissionFilterMixin, GroupAdmin):
+    pass
+
 class ProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = _("profiel")
 
-class UserAdmin(UserAdmin):
+class UserAdmin(PermissionFilterMixin, UserAdmin):
     inlines = (ProfileInline, )
 
-# Re-register UserAdmin
+# Re-register Group and User Admins
+admin.site.unregister(Group)
+admin.site.register(Group, MyGroupAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+
 
 # Remove this
 admin.site.unregister(Site)
