d2c0c5e15980142eccc35569559f60a9a2c5f757
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-12-21 14:48:26 +0100

    Fix #45 - Allow changing password

--- openebs2/urls.py
+++ openebs2/urls.py
@@ -12,8 +12,11 @@ handler500 = TemplateView.as_view(template_name="openebs/servererror.html")
 urlpatterns = patterns('',
     url(r'^', include('openebs.urls')),
     url(r'^', include('kv1.urls')),
+
     url(r'^inloggen/$', 'django.contrib.auth.views.login', {'template_name': 'users/login.html'}, name="app_login"),
     url(r'^uitloggen/$', 'django.contrib.auth.views.logout_then_login', name="app_logout"),
+    url(r'^wachtwoord/wijzigen$', 'django.contrib.auth.views.password_change', {'template_name': 'users/password_change_form.html'}, name="app_password_change"),
+    url(r'^wachtwoord/gewijzigd$', 'django.contrib.auth.views.password_change_done', {'template_name': 'users/password_change_done.html'}, name="app_password_changed"),
     url(r'^geweigerd/$', TemplateView.as_view(template_name="openebs/nopermission.html"), name="app_nopermission"),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
