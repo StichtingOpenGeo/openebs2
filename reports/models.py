44da7447361bb2c82e5a90a3d9df568aa9ce1880
Joel Haasnoot <joelhaasnoot@gmail.com>
2014-06-06 13:03:12 +0200

    Change percentage and sort by it

--- reports/models.py
+++ reports/models.py
@@ -21,7 +21,7 @@ class Kv6Log(models.Model):
 
     @staticmethod
     def do_report():
-        qry = """select l.id, l.publiclinenumber, l.lineplanningnumber, coalesce(count(lg.id), 0) as seen, count(*) as planned
+        qry = """select l.id, l.publiclinenumber, l.lineplanningnumber, coalesce(count(lg.id), 0) as seen, count(*) as planned, (coalesce(count(lg.id), 0)/count(*)*100) as percentage
         from kv1_kv1journey j
         join kv1_kv1line l on (j.line_id = l.id)
         join kv1_kv1journeydate jd on (jd.journey_id = j.id and jd.date = CURRENT_DATE)
@@ -29,11 +29,24 @@ class Kv6Log(models.Model):
         WHERE j.dataownercode = 'HTM' and
         j.departuretime between ROUND(EXTRACT(EPOCH FROM CURRENT_TIME - INTERVAL '30 MIN')) and ROUND(EXTRACT(EPOCH FROM CURRENT_TIME))
         group by l.id, l.lineplanningnumber
-        order by l.lineplanningnumber::int;"""
+        order by percentage, l.lineplanningnumber::int;"""
 
         cursor = connection.cursor()
         cursor.execute(qry)
         return dictfetchall(cursor)
+
+    @staticmethod
+    def do_details():
+        qry = """SELECT l.id, l.dataownercode, l.lineplanningnumber, j.journeynumber, j.departuretime, lg.id, lg.max_punctuality, lg.vehiclenumber
+        FROM kv1_kv1journey j
+        JOIN kv1_kv1line l ON (j.line_id = l.id)
+        JOIN kv1_kv1journeydate jd ON (jd.journey_id = j.id and jd.date = CURRENT_DATE)
+        LEFT OUTER JOIN reports_kv6log lg ON (j.journeynumber = lg.journeynumber and jd.date = lg.operatingday and lg.lineplanningnumber = l.lineplanningnumber)
+        WHERE j.dataownercode = 'HTM' and
+        j.departuretime < ROUND(EXTRACT(EPOCH FROM CURRENT_TIME))
+        ORDER BY l.lineplanningnumber::int, j.departuretime;"""
+
+
 
 def dictfetchall(cursor):
     desc = cursor.description
