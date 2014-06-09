6f356960efe517e47e99694c7ac6c2b7f41b0ad3
Joel Haasnoot <joelhaasnoot@gmail.com>
2014-06-09 10:58:24 +0200

    Add an index to departuretime, see if it makes things faster

--- kv1/models.py
+++ kv1/models.py
@@ -59,7 +59,7 @@ class Kv1Journey(models.Model):
     line = models.ForeignKey(Kv1Line, related_name="journeys")  # Represent lineplanningnumber\
     journeynumber = models.PositiveIntegerField(max_length=6)  # 0 - 999999
     scheduleref = models.PositiveIntegerField() # Field 'availabilityconditionref'
-    departuretime = models.PositiveIntegerField() # In seconds
+    departuretime = models.PositiveIntegerField(db_index=True) # In seconds
     duration = models.PositiveIntegerField() # Also in seconds
     direction = models.PositiveSmallIntegerField()
 
