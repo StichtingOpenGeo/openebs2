4fd3bdefbeda86fb063d3f2487dc2f7f14d36117
Joel Haasnoot <joelhaasnoot@gmail.com>
2014-06-09 12:46:33 +0200

    Fix linenumber -> lineid

--- reports/models.py
+++ reports/models.py
@@ -35,9 +35,9 @@ class Kv6Log(models.Model):
         journey_list = dictfetchall(cursor)
         output = { }
         for journey in journey_list:
-            line = journey['lineplanningnumber']
+            line = journey['line_id']
             if line not in output:
-                output[line] = { 'lineplanningnumber': line, 'publiclinenumber': journey['publiclinenumber'],
+                output[line] = { 'line_id': line, 'publiclinenumber': journey['line_id'],
                                  'list': [], 'live': [], 'seen': 0, 'expected': 0 }
             output[line]['list'].append(journey)
             output[line]['expected'] += 1
@@ -47,7 +47,7 @@ class Kv6Log(models.Model):
             output[line]['percentage'] = round((float(output[line]['seen']) / float(output[line]['expected'])) * 100, 1)
 
         list = sorted(output.values(), key= lambda k: k['percentage'])
-        return sorted(list, key= lambda k: int(k['lineplanningnumber']))
+        return sorted(list, key= lambda k: int(k['line_id']))
 
 
 def dictfetchall(cursor):
