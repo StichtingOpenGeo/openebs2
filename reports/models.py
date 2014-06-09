18c288f71dd6ed3bae5b4f847c1071ebd414bbcc
Joel Haasnoot <joelhaasnoot@gmail.com>
2014-06-09 13:08:00 +0200

    Change timeblock search

--- reports/models.py
+++ reports/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.db import models, connection
 from kv1.models import Kv1Line
 from kv15.enum import DATAOWNERCODE
@@ -23,13 +23,15 @@ class Kv6Log(models.Model):
     @staticmethod
     def do_report():
         now = datetime.now()
+        fifteen = datetime.now() - timedelta(minutes=15)
         qry = """SELECT l.lineplanningnumber, j.journeynumber, lg.id as log_id, lg.vehiclenumber, lg.last_logged, lg.last_punctuality
         FROM kv1_kv1journey j
         JOIN kv1_kv1line l on (j.line_id = l.id)
         JOIN kv1_kv1journeydate jd ON (jd.journey_id = j.id and jd.date = CURRENT_DATE)
         LEFT OUTER JOIN reports_kv6log lg ON (j.journeynumber = lg.journeynumber and jd.date = lg.operatingday)
-        WHERE j.dataownercode = 'HTM' AND %s BETWEEN j.departuretime and j.arrivaltime
-        ORDER BY j.line_id;""" % ((now.hour*60*60)+(now.minute*60)+now.second)
+        WHERE j.dataownercode = 'HTM' AND j.departuretime < %s and j.arrivaltime > %s
+        ORDER BY j.line_id;""" % (((now.hour*60*60)+(now.minute*60)+now.second),
+                                  ((fifteen.hour*60*60)+(fifteen.minute*60)+fifteen.second))
 
         cursor = connection.cursor()
         cursor.execute(qry)
