2b445dc544995f0570905bbc096c1a2e18e8d911
Joel Haasnoot <joelhaasnoot@gmail.com>
2015-04-06 13:37:00 +0200

    Change stops to all be imported, regardless if in line diagrams or not
    Change is needed to support import lots of external messages, we can't assume the line diagrams are up to date.

--- kv1/management/commands/import_rid.py
+++ kv1/management/commands/import_rid.py
@@ -19,13 +19,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.folder = args[0]
         print "STEP 1: Lines"
-        stops = self.do_lines()
-        self.log("Got %s stops to process in next step" % len(stops))
+        self.do_lines()
         print "==========================\n\nSTEP 2: Stops"
-        self.do_stops(stops)
+        self.do_stops()
 
     def do_lines(self):
-        stops = []
         with open(self.folder+'/openebs_lines.csv') as routes_file:
             routes_reader = csv.DictReader(routes_file, delimiter=',', quotechar='"')
             for row in routes_reader:
@@ -36,32 +34,21 @@ class Command(BaseCommand):
                     line.headsign = row['name']
                     line.publiclinenumber = row['publiccode']
                     line.save()
-                    for line_row in line.stop_map:
-                        if 'left' in line_row and line_row['left'] is not None:
-                            left = line_row['left']['id'].replace('_', ':')
-                            if left not in stops:
-                                stops.append(left)
-                        if 'right' in line_row and line_row['right'] is not None:
-                            right = line_row['right']['id'].replace('_', ':')
-                            if right not in stops:
-                                stops.append(right)
                 else:
                     self.log("Failed to match route: %s" % row['operator_id'])
 
-        return stops
 
-    def do_stops(self, stops):
+    def do_stops(self):
         with open(self.folder+'/openebs_stops.csv') as stops_file:
            stop_reader = csv.DictReader(stops_file, delimiter=',', quotechar='"')
            i = 0
            for row in stop_reader:
-               if row['operator_id'] in stops:
-                   stop_code = row['operator_id'].split(':')
-                   s, created = Kv1Stop.objects.get_or_create(dataownercode=stop_code[0], userstopcode=stop_code[1])
-                   s.name = row['name']
-                   s.location = Point(float(row['longitude']), float(row['latitude']), srid=4326)
-                   s.timingpointcode = row['timingpointcode']
-                   s.save()
+               stop_code = row['operator_id'].split(':')
+               s, created = Kv1Stop.objects.get_or_create(dataownercode=stop_code[0], userstopcode=stop_code[1])
+               s.name = row['name']
+               s.location = Point(float(row['longitude']), float(row['latitude']), srid=4326)
+               s.timingpointcode = row['timingpointcode']
+               s.save()
                i = i+1
                if i % 100 == 0:
                    self.log("Did %s stops" % i)
