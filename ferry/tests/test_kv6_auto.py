5d3a2f73b0b9d2ff84b622765f3db6332e72fa7c
Joel Haasnoot <joelhaasnoot@gmail.com>
2016-04-15 16:37:36 +0200

    Fix test for sending automatic messages (default scenario)

--- ferry/tests/test_kv6_auto.py
+++ ferry/tests/test_kv6_auto.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from django.contrib.gis.geos import Point
+from django.core.management import call_command
 from django.test import TestCase
 
 from ferry.management.commands.sendkv6 import Command as SendKv6Command
@@ -12,7 +13,6 @@ class TestFerryKv6Messages(TestCase):
     @classmethod
     def setUpClass(cls):
         super(TestFerryKv6Messages, cls).setUpClass()
-        cls.testClass = SendKv6Command()
 
         # Setup data
         cls.stop_depart = Kv1Stop(dataownercode="WSF", userstopcode=1, location=Point(1, 1))
@@ -29,7 +29,7 @@ class TestFerryKv6Messages(TestCase):
         j1.save()
         j2 = Kv1Journey(dataownercode="WSF", line=cls.line, journeynumber=2, direction=0, scheduleref=1, departuretime=cls.calc(datetime.now()-timedelta(minutes=19)))
         j2.save()
-        j3 = Kv1Journey(dataownercode="WSF", line=cls.line, journeynumber=3, direction=1, scheduleref=1, departuretime=cls.calc(datetime.now()-timedelta(minutes=6)))
+        j3 = Kv1Journey(dataownercode="WSF", line=cls.line, journeynumber=3, direction=1, scheduleref=1, departuretime=cls.calc(datetime.now()+timedelta(minutes=4)))
         j3.save()
         j4 = Kv1Journey(dataownercode="WSF", line=cls.line, journeynumber=4, direction=0, scheduleref=1, departuretime=cls.calc(datetime.now()+timedelta(hours=2)))
         j4.save()
@@ -46,23 +46,23 @@ class TestFerryKv6Messages(TestCase):
         self.ferry.enable_auto_messages=False
         self.ferry.save()
         self.assertEqual(0, FerryKv6Messages.objects.count())
-        self.testClass.handle()
+        call_command('sendkv6')
         self.assertEqual(0, FerryKv6Messages.objects.count())
 
         self.ferry.enable_auto_messages=True
         self.ferry.save()
-        self.testClass.handle()
+        call_command('sendkv6')
         self.assertEqual(3, FerryKv6Messages.objects.count())
 
     def test_send_initial(self):
         self.assertEqual(0, FerryKv6Messages.objects.count())
 
-        self.testClass.handle()
+        call_command('sendkv6')
         self.assertEqual(3, FerryKv6Messages.objects.count())
         msgs = FerryKv6Messages.objects.all().order_by('created')
         self.assertEqual(FerryKv6Messages.Status.ARRIVED, msgs[0].status)
         self.assertEqual(FerryKv6Messages.Status.DEPARTED, msgs[1].status)
-        self.assertEqual(FerryKv6Messages.Status.INITIALIZED, msgs[2].status)
+        self.assertEqual(FerryKv6Messages.Status.READY, msgs[2].status)
 
         # Other messages haven't been created, verified by counting
 
@@ -70,7 +70,7 @@ class TestFerryKv6Messages(TestCase):
         self.assertEqual(0, FerryKv6Messages.objects.count())
         FerryKv6Messages(ferry=self.ferry, operatingday=datetime.now(), journeynumber=3, delay=300).save()
 
-        self.testClass.handle()
+        call_command('sendkv6')
         self.assertEqual(2, FerryKv6Messages.objects.count())
         msgs = FerryKv6Messages.objects.all().order_by('created')
         self.assertEqual(FerryKv6Messages.Status.ARRIVED, msgs[0].status)
