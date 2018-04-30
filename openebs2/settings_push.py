31cfa528355c38e1545958439f2d6d89f23195c7
Joel Haasnoot <joelhaasnoot@gmail.com>
2018-04-30 09:33:25 +0200

    Cleanup settings

--- openebs2/settings_push.py
+++ openebs2/settings_push.py
@@ -2,29 +2,18 @@ PUSH_DEFAULTS = {
     'timeout': 10,  # Seconds
     'debug': True  # Logs everything
 }
-PUSH_SETTINGS = [
-    {'alias': 'govi',
-     'enabled': True,
-     'priority': 1,
-     'failOnFailure': True,
-     'host': 'localhost:8888',  #drisacc.transmodel.nl
-     'subscriberName': 'openov',
-     'endpoints': {
-        'KV6': {'path': '/TMI_Post/KV6'},
-        'KV15': {'path': '/TMI_Post/KV15'},
-        'KV17': {'path': '/TMI_Post/KV17'}
-     }
-    },
-    {'alias': 'ovapi',
-     'enabled': False,
-     'priority': 2,
-     'failOnFailure': False,
-     'host': 'ovrkv.ovapi.nl',
-     'subscriberName': 'openov',
-     'endpoints': {
-         'KV15': {'path' : '/RIG/KV6posinfo'},
-         'KV15': {'path' : '/RIG/KV15messages'},
-         'KV17': {'path' : '/RIG/KV17cvlinfo'}
-     }
-    }
-]
+
+# EXAMPLE CONFIG
+# {'alias': 'govi',
+#  'enabled': True,
+#  'priority': 1,
+#  'failOnFailure': True,
+#  'host': 'drisacc.transmodel.nl',
+#  'subscriberName': 'openov',
+#  'endpoints': {
+#     'KV6': {'path': '/TMI_Post/KV6'},
+#     'KV15': {'path': '/TMI_Post/KV15'},
+#     'KV17': {'path': '/TMI_Post/KV17'}
+#  }
+
+PUSH_SETTINGS = []
