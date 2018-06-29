12a7d2359756c82b9626bd79ba9de418a4bad6c3
Joel Haasnoot <joelhaasnoot@gmail.com>
2018-06-29 09:00:11 +0200

    Change logging

--- openebs/management/commands/batch_resend_current.py
+++ openebs/management/commands/batch_resend_current.py
@@ -24,13 +24,13 @@ class Command(BaseCommand):
             if trip.is_cancel:
                 res = trip.to_xml()
                 if res is not None:
-                    print ("Resending: %s on %s" % (trip.realtime_id(), self.date))
+                    self.log("Resending: %s on %s" % (trip.realtime_id(), self.date))
                     to_send.append(res)
                     to_send_trips.append(trip)
                 else:
-                    print ("ERROR!: %s on %s" % (trip.realtime_id(), self.date))
+                    self.log("ERROR!: %s on %s" % (trip.realtime_id(), self.date))
             else:
-                print ("Not cancelled: %s on %s" % (trip.realtime_id(), self.date))
+                self.log("Not cancelled: %s on %s" % (trip.realtime_id(), self.date))
             if len(to_send) > 0 and len(to_send) % self.BATCH_SIZE == 0:
                 to_send, to_send_trips = self.send(to_send, to_send_trips)
 
@@ -38,12 +38,15 @@ class Command(BaseCommand):
             self.send(to_send, to_send_trips)
 
     def send(self, to_send, to_send_trips):
-        self.stdout.write("Sending batch of %s" % len(to_send))
         start = datetime.now()
         success = self.pusher.push_message(to_send)
-        self.stdout.write("Took %s seconds" % (datetime.now() - start).seconds)
-        if not success:
-            self.stdout.write("Failed to send batch! %s" % to_send_trips)
+        if success:
+            self.log("Sending batch of %s, took %s seconds" % (len(to_send), (datetime.now() - start).seconds))
+        else:
+            self.log("Failed to send batch! %s" % to_send_trips)
         to_send = []
         to_send_trips = []
         return to_send, to_send_trips
+
+    def log(self, msg):
+        self.stdout.write("%s - %s", (datetime.now().isoformat(), msg))
