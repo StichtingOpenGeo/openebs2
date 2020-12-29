c9320b2d001990fc04d8e55f1ae2d89802dc019b
baasje85 <baas@bliksemlabs.com>
2020-12-29 14:12:41 +0100

    cleaned files

--- openebs2/static/js/import_msg.js
+++ openebs2/static/js/import_msg.js
@@ -21,7 +21,7 @@ function getMessage() {
     var line = messagestarttime.split(' ')[0].replace(/(\d{4})-(\d\d)-(\d\d)/, '$3-$2-$1') + ' '+messagestarttime.split(' ')[1].slice(0,-3) + ' - '// reformat to dd-mm-yyyy HH:mm
     var messageendtime = $title.find('messageendtime').text().replace('t', ' ').split("+")[0];
     if (messageendtime.length > 1) {
-        var endtime = new Date(Date.parse(messageendtime, "dd/mm/yyyy HH:mm")); // niet verplicht, wel handig om data te pakken als die is ingevuld
+        var endtime = new Date(Date.parse(messageendtime, "dd/mm/yyyy HH:mm"));
         var starttime = new Date(Date.parse(messagestarttime, "dd/mm/yyyy HH:mm"));
 
         var start = Date.UTC(starttime.getFullYear(), starttime.getMonth(), starttime.getDate());
@@ -35,34 +35,13 @@ function getMessage() {
     }
     $('#geldigheid-regel').text(line);
 
-    //    if (Date(messageendtime - messagestarttime)/(1000*60*60*24) ;
-
-    //} else {
-
-   // }
-
-    /*
-    {{ msg.messagestarttime|date:"d-m-Y H:i" }} -
-    {% if msg.get_message_duration == 0 %}
-        {{ msg.messageendtime|date:"H:i" }}
-    {% elif msg.get_message_duration == 1 %}
-        {{ msg.messageendtime|date:"H:i" }} <span title="{% trans 'Volgende dag' %}"><em>(+1)</em></span>
-    {% elif msg.get_message_duration > 1 and msg.get_message_duration < 365 %}
-        {{ msg.messageendtime|date:"d-m-Y H:i" }}
-    {% else %}
-        <span title="{% trans 'Tot bericht wordt verwijderd' %}"><i class="glyphicon glyphicon-trash"></i></span>
-    {% endif %}
-    */
     var userstopcodes = $title.find('userstopcodes').text();
     var messagepriority = $title.find('messagepriority').text();
     var messagetype = $title.find('messagetype').text();
     var messagedurationtype = $title.find('messagedurationtype').text();
 
-    var messagetimestamp = $title.find('messagetimestamp').text(); // verplicht, maar niet zichtbaar in 'detailview'
+    var messagetimestamp = $title.find('messagetimestamp').text();
 
-    // AJAX schrijven op haltenamen erbij te zoeken om halte-overzichtje te krijgen
-
-    // Volgende rijtje misschien tonen indien ingevuld? --> zijn niet verplicht
     reasontype = $title.find('reasontype').text();
     subreasontype = $title.find('subreasontype').text();
     reasoncontent = $title.find('reasoncontent').text();
