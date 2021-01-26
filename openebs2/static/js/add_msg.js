4c234f606c0a4b7b794bb784bc4e5ff420bb3347
Baasje85 <51262792+Baasje85@users.noreply.github.com>
2021-01-26 13:38:27 +0100

    Separated Kv17 from Kv15 (#142)
    * update master
    
    * Fix #61
    
    * Separated Kv17 from Kv15 in add_msg.js
    
    * Separated Kv17 from Kv15 in form.py
    
    * remove recovered trip from database
    
    * fix writeLine for message fired stops twice.
    update fadeIn-time writeList changed to 200
    
    * fix writeTrips for 'ritaanpassing' fired journeys twice
    
    Co-authored-by: Stefan de Konink <stefan@konink.de>

--- openebs2/static/js/add_msg.js
+++ openebs2/static/js/add_msg.js
@@ -63,8 +63,6 @@ function selectStop(event, ui) {
     }
 }
 
-
-
 function selectStopFromBall(obj) {
     $('#halte-list .help').remove()
     var did = false
@@ -259,139 +257,7 @@ function writeHaltesWithMessages(data, status) {
     });
 }
 
-/* TRIP SELECTION */
-var selectedTrips = [];
-var activeJourneys = [];
 
-function showTrips(event) {
-    $("#rows tr.success").removeClass('success');
-    $(".suc-icon").remove();
-    $(this).children('td').eq(1).append('<span class="suc-icon pull-right glyphicon glyphicon-arrow-right"></span>');
-    $.ajax('/line/'+$(this).attr('id').substring(1)+'/ritten', {
-        success : writeTrips
-    })
-    $('#line').val($(this).attr('id').substring(1))
-    $(this).addClass('success')
-}
-
-function loadPreselectedJourneys() {
-    $(".rit-preload").each(function(id, val) {
-        selectedTrips.push($(val).attr('id').substring(2));
-    });
-    writeTripList();
-}
-
-function selectTrip(event, ui) {
-    var ritnr = $(ui.selected).attr('id').substring(1);
-    if ($.inArray(parseInt(ritnr), activeJourneys) != -1) /* Note our array stores numbers, so convert */
-        return;
-
-    var id = $.inArray(ritnr, selectedTrips);
-    if (id == -1) {
-        $('#rit-list .help').hide();
-        $(ui.selected).addClass('success');
-        var label = $(ui.selected).find("strong").text();
-        var dellink = '<span class="trip-remove glyphicon glyphicon-remove"></span>';
-        $('#rit-list').append('<span id="st'+ritnr+'" class="pull-left trip-selection label label-danger">'+label+' '+dellink+'</span>');
-        selectedTrips.push(ritnr);
-        writeTripList();
-    } else {
-        removeTrip($(ui.selected).attr('id').substring(1));
-    }
-}
-
-function removeTripFromX(event, ui) {
-    removeTrip($(this).parent().attr('id').substring(2));
-}
-
-function removeTrip(ritnr) {
-    var id = $.inArray(ritnr, selectedTrips);
-    if (id != -1) {
-        $('#t'+ritnr).removeClass('success');
-        $('#st'+ritnr).remove();
-        selectedTrips.splice(id, 1);
-    }
-    if (selectedTrips.length == 0) {
-        $('#rit-list .help').show();
-    }
-    writeTripList();
-}
-
-function writeTripList() {
-    var out = "";
-    $.each(selectedTrips, function(index, val) {
-        out += val+',';
-    });
-    $("#journeys").val(out)
-}
-
-function writeTrips(data, status) {
-    $('#trips tbody').fadeOut(200).empty();
-    tripRows = null
-    maxLen = Math.max(data.object.trips_1.length, data.object.trips_2.length)
-    for (i = 0; i <= maxLen; i = i + 1) {
-        a = null
-        b = null
-        if (i in data.object.trips_1)
-            a = data.object.trips_1[i]
-        if (i in data.object.trips_2)
-            b = data.object.trips_2[i]
-        tripRows += renderTrip(a, b);
-    }
-    $('#trips tbody').append(tripRows)
-    $('#trips tbody').fadeIn(200);
-}
-
-function renderTrip(trip_a, trip_b) {
-    out = '<tr>';
-    out += renderTripCell(trip_a);
-    out += renderTripCell(trip_b);
-    out += '</tr>';
-    return out
-}
-
-function renderTripCell(trip) {
-    if (trip == null)
-        return "<td>&nbsp;</td>";
-
-    if ($.inArray(trip.id, activeJourneys) != -1) {
-        out = '<td class="trip warning" id="t'+trip.id+'">'
-    } else {
-        out = '<td class="trip" id="t'+trip.id+'">'
-    }
-    out += "<strong>Rit "+trip.journeynumber+"</strong>"
-    out += "&nbsp;<small>Vertrek "+convertSecondsToTime(trip.departuretime)+"</small>"
-    if ($.inArray(trip.id, activeJourneys) != -1) {
-        out += '<span class="glyphicon glyphicon-warning-sign pull-right" title="Rit is al opgeheven"></span>'
-    }
-    out += "</td>"
-    return out
-}
-
-function convertSecondsToTime(seconds) {
-    var hours   = Math.floor(seconds / 3600);
-    var minutes = Math.floor((seconds - (hours * 3600)) / 60);
-    var extra = "";
-    if (hours > 23) {
-        hours = hours - 24
-        extra = " <em>(+1)</em>"
-    }
-    return ""+padTime(hours)+":"+padTime(minutes)+extra;
-}
-
-function getActiveJourneys() {
-     $.ajax('/ritaanpassing/ritten.json', {
-            success : writeActiveJourneys
-     })
-}
-
-function writeActiveJourneys(data, status) {
-    if (data.object) {
-        $.each(data.object, function (i, journey) {
-            activeJourneys.push(journey.journey_id)
-        });
-    }
-}
 
 /* TIME FUNCTIONS */
 function checkMessageTime(event, ui) {
