2f513a8777765c8f1481dd762e12460491cdd17d
Baasje85 <51262792+Baasje85@users.noreply.github.com>
2023-04-06 14:00:30 +0200

    added try/except + logging (#211)

--- utils/push.py
+++ utils/push.py
@@ -86,10 +86,13 @@ class Push:
                 self.log.error("Got exception while connecting to %s: %s" % (self.alias, ex))
 
             if not error and conn is not None:
-                response = conn.getresponse()
-                response_code = response.status
-                response_content = response.read().decode('utf-8')
-                conn.close()
+                try:
+                    response = conn.getresponse()
+                    response_code = response.status
+                    response_content = response.read().decode('utf-8')
+                    conn.close()
+                except Exception as ex:
+                    self.log.error("Got exception while waiting for response from %s: %s" % (self.alias, ex))
 
             if self.debug:
                 self.log.debug("Connecting to %s and got response code %s and content: %s" % (
